1718865c98b73cf387eb96cbec48f35e11c12bdc
NCJ <iamncj@outlook.com>
2020-07-12 11:59:22 +0800

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	// filter
 	filter := sensitive.New()
 	err := filter.LoadWordDict("dict.txt")
 	if err != nil {
@@ -14,22 +15,27 @@ func main() {
 		_ = filter.LoadNetWordDict("https://raw.githubusercontent.com/importcjj/sensitive/master/dict/dict.txt")
 	}
 
+	// gin
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		buf := make([]byte, 1024)
 		n, _ := c.Request.Body.Read(buf)
 		body := string(buf[0:n])
 		fmt.Print(body)
+
 		res, illegalWord := filter.Validate(body)
 		filtered := body
 		if !res {
 			filtered = filter.Filter(body)
 		}
+
 		c.JSON(200, gin.H{
 			"valid":       res,
 			"illegalWord": illegalWord,
 			"filtered":    filtered,
 		})
 	})
+
+	// run
 	_ = r.Run() // listen and serve on 0.0.0.0:8080
 }
